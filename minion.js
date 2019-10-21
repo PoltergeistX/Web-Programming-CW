944d9fd24e67d6f64743f5dd5eed8336da758368
Poltergeist <55681261+PoltergeistX@users.noreply.github.com>
2019-10-21 06:41:39 +0400

    Update minion.js

--- minion.js
+++ minion.js
@@ -1,8 +1,34 @@
-class minion{
-    var health;
-    var dps ;
-    var level = 1;
 
     
+const HEALTH = 5;
+const DAMAGE = 5;
+const SPEEDMAX = 0.6;
+const SPEEDMIN = 0.4;
+const DRAG = 0.925;    
 
-}
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+var fps = "FPS";
+var times = [];
+var paused = false;
+var pauseKey = 27;
+var keysDown = [];
+
+var player;
+var zombies = [];
+
+function Zombie (_x, _y)
+{
+	this.pos = { x: _x, y: _y };
+	this.vel = { x: 0, y: 0 };
+	this.dir = 0;
+
+	this.alive = true;
+	this.health = HEALTH;
+    this.speed = Math.random() * (SPEEDMAX - SPEEDMIN) + SPEEDMIN;
+    
+    this.update = function(){
+        // Follow player
+			this.dir = Math.atan((player.pos.y - this.pos.y) / (player.pos.x - this.pos.x));
+			if (player.pos.x - this.pos.x < 0)
+				this.dir += Math.PI;
